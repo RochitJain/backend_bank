62b69fae3d106d1356a4ef12006f7950f3979218
RochitJain <rochitjain5261@gmail.com>
2026-04-29 14:15:00 +0530

    feat: added rate limiter

--- src/app.js
+++ src/app.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const app = express();
+const {rateLimit} = require('express-rate-limit')
 const userRouter = require('./routes/user.routes')
 const accountRouter = require('./routes/account.routes')
 const cookieParser = require('cookie-parser')
@@ -9,6 +10,15 @@ const transactionRouter = require('./routes/transaction.routes')
 app.use(express.json())
 app.use(cookieParser())
 
+
+const limiter = rateLimit({
+	windowMs: 15 * 60 * 1000, // 15 minutes
+	limit: 100, // Limit each IP to 100 requests per `window` (here, per 15 minutes).
+	standardHeaders: 'draft-8', // draft-6: `RateLimit-*` headers; draft-7 & draft-8: combined `RateLimit` header
+	legacyHeaders: false, // Disable the `X-RateLimit-*` headers.
+	ipv6Subnet: 56, // Set to 60 or 64 to be less aggressive, or 52 or 48 to be more aggressive
+})
+app.use(limiter)
 app.get('/',(req,res)=>{res.send('Project is Live')})
 app.use('/api/auth', userRouter)
 app.use('/api/account', accountRouter)
